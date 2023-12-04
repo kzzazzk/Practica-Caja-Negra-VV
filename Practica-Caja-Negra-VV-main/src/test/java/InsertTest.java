e7969569906be4d92117bf7a81786dd599799034
kzzazzk <zsahraoui20@gmail.com>
2023-12-04 02:46:29 +0100

    InsertTest.java done

--- Practica-Caja-Negra-VV-main/src/test/java/InsertTest.java
+++ Practica-Caja-Negra-VV-main/src/test/java/InsertTest.java
@@ -1,7 +1,26 @@
 import static org.junit.jupiter.api.Assertions.*;
+import com.bst.BST;
 
-class InsertTest{
-    BST bst;
-    assertEquals(
-    )
+import com.exceptions.DepthException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class InsertTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/InsertTest.csv", numLinesToSkip = 1, delimiterString = ",")
+    @DisplayName("Test Insertion with Content: {0}, Success: {1}")
+    void testInsertInt(int content, boolean recursive, boolean expectedSuccess) {
+        BST<Integer> bst = new BST<>();
+        try {
+            bst.insert(content, recursive);
+            assertTrue(expectedSuccess);
+        } catch (DepthException e) {
+            assertEquals("DepthException", e.getClass().getSimpleName());
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("IllegalArgumentException", e.getClass().getSimpleName());
+        }
+    }
 }
